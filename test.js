61f6f6f1e424756a1ad28d2ccb3b4cbbe9860a53
Sukhdev <48499132+algojedi@users.noreply.github.com>
2024-03-12 10:19:24 -0400

    random names printer

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 const phrase = "hello world my name is Asher";
 
+const names = ['asher', 'chloe', 'mama', 'dada']
+
+// Function to select a random name
+function selectRandomName(namesArray) {
+  var randomIndex = Math.floor(Math.random() * namesArray.length);
+  return namesArray[randomIndex];
+}
+
+// Call the function to select a random name
+const randomName = selectRandomName(names);
+
+// Display the random name
 function myName() {
-    console.log(phrase)
+    console.log("Randomly selected name:", randomName);
 }
 
 myName();
